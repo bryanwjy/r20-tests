48b91b9e0e906f622f44b929b5e38057dd40c04a
Bryan Wong <wongjengyan@gmail.com>
2025-07-11 01:31:04 +0900

    enumerate-view

--- gcc/enumerate_view.pass.cpp
+++ gcc/enumerate_view.pass.cpp
@@ -19,6 +19,7 @@
 // <http://www.gnu.org/licenses/>.
 
 #include "rxx/enumerate_view.h"
+
 #include "rxx/get_element.h"
 #include "test_iterators.h"
 
@@ -29,6 +30,8 @@
 
 namespace ranges = std::ranges;
 namespace views = std::views;
+namespace xranges = rxx::ranges;
+namespace xviews = rxx::views;
 
 using rxx::tests::test_bidirectional_range;
 using rxx::tests::test_forward_range;
@@ -37,7 +40,7 @@ using rxx::tests::test_random_access_range;
 
 constexpr bool test01() {
     int x[] = {1, 2, 3};
-    auto v = x | rxx::views::enumerate;
+    auto v = x | xviews::enumerate;
 
     assert(ranges::equal(v | views::keys, (int[]){0, 1, 2}));
     assert(ranges::equal(v | views::values, (int[]){1, 2, 3}));
@@ -58,7 +61,7 @@ template <template <class> class Container>
 void test02() {
     int x[] = {1, 2, 3};
     Container<int> rx(x);
-    auto v = rx | rxx::views::enumerate;
+    auto v = rx | xviews::enumerate;
 
     int j = 0;
     for (auto [i, y] : v) {
@@ -66,10 +69,10 @@ void test02() {
         assert(j == i);
         ++j;
     }
-    assert(j == ranges::size(x));
+    assert(j == xranges::size(x));
 
-    if constexpr (ranges::bidirectional_range<decltype(rx)>) {
-        static_assert(ranges::bidirectional_range<decltype(v)>);
+    if constexpr (xranges::bidirectional_range<decltype(rx)>) {
+        static_assert(xranges::bidirectional_range<decltype(v)>);
         for (auto [i, y] : v | views::reverse) {
             --j;
             assert(&y == &x[j]);
@@ -78,14 +81,14 @@ void test02() {
         assert(j == 0);
     }
 
-    if constexpr (ranges::random_access_range<decltype(rx)>) {
-        static_assert(ranges::random_access_range<decltype(v)>);
-        for (j = 0; j < ranges::ssize(x); ++j) {
-            assert(rxx::ranges::get_element<0>(v[j]) == j);
-            assert(&rxx::ranges::get_element<1>(v[j]) == &x[j]);
+    if constexpr (xranges::random_access_range<decltype(rx)>) {
+        static_assert(xranges::random_access_range<decltype(v)>);
+        for (j = 0; j < xranges::ssize(x); ++j) {
+            assert(xranges::get_element<0>(v[j]) == j);
+            assert(&xranges::get_element<1>(v[j]) == &x[j]);
             assert(*(v.begin() + j) == v[j]);
-            assert(*(v.begin() + (ranges::size(x) - 1) - j) ==
-                v[ranges::size(x) - 1 - j]);
+            assert(*(v.begin() + (xranges::size(x) - 1) - j) ==
+                v[xranges::size(x) - 1 - j]);
             assert(v.begin() + j + 1 > v.begin() + j);
             assert(v.begin() + j < v.begin() + j + 1);
             assert(v.begin() + j >= v.begin());
@@ -101,8 +104,8 @@ void test02() {
 void test_lwg3912() {
     int x[] = {1, 2, 3};
     test_input_range<int> rx(x);
-    auto v = rx | rxx::views::enumerate;
-    auto iter = std::ranges::begin(v);
+    auto v = rx | xviews::enumerate;
+    auto iter = xranges::begin(v);
     // LWG 3912. enumerate_view::iterator::operator- should be noexcept
     static_assert(noexcept(iter - iter));
 }
