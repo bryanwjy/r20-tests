f0c9d6147fba86d2fe38d8144652758146dbd602
Bryan Wong <wongjengyan@gmail.com>
2025-07-20 02:01:28 +0900

    copyright

--- gcc/numeric/ranges.iota.pass.cpp
+++ gcc/numeric/ranges.iota.pass.cpp
@@ -1,9 +1,22 @@
-// { dg-do run { target c++23 } }
+// Copyright 2025 Bryan Wong
+// Adapted from GCC testsuite
 
-#include "rxx/numeric/iota.h"
+// Copyright (C) 2020-2025 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 
 #include "../test_iterators.h"
 #include "rxx/algorithm/equal.h"
+#include "rxx/numeric/iota.h"
 
 #include <cassert>
 
